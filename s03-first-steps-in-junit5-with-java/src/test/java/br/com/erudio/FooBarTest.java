83857de4f05acf463f1ccca389b3a1be69a26877
arturbaccarin <baccarin.artur@gmail.com>
2024-05-02 07:10:18 -0300

    first commit

---
+++ s03-first-steps-in-junit5-with-java/src/test/java/br/com/erudio/FooBarTest.java
@@ -0,0 +1,13 @@
+package br.com.erudio;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+class FooBarTest {
+
+    @Test
+    void test() {
+        System.out.println("Hello JUnit 5");
+    }
+}
